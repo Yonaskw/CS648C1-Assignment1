ee22a0906751abbc83faf4055ff3b11e8faf8ea4
Yonaskw <90166490+Yonaskw@users.noreply.github.com>
2022-01-31 05:35:55 -0800

    first

--- public/App.js
+++ public/App.js
@@ -4,6 +4,8 @@ const element = /*#__PURE__*/React.createElement("div", {
       id:"img",
     src: "imgs/yl_profile.jpeg"
   }), /*#__PURE__*/React.createElement("p", null, "Hello, my name is Yonas Lemmi. I am currently enrolled in Advanced Certificate in Web and Mobile Applications Development at SDSU. I am pursuing a masters in Computer Science, hopefully I can start in fall 2022.  "), /*#__PURE__*/React.createElement("a", {
+    className: "bttn",
     href: "https://github.com/Yonaskw"
-  }, "Please Click here to access my Github"));
+  }, React.createElement("bttn", null, "Please Click Here to access my github!")));
   ReactDOM.render(element, document.getElementById('contents'));
+
