a2949386604ea68529cc5d817de7056317838476
Yonaskw <90166490+Yonaskw@users.noreply.github.com>
2022-01-31 01:45:57 -0800

    first

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+const element = (
+    <div title="Outer div">
+      <h1>Yonas Lemmi</h1>
+      
+      <p>Hello, my name is Yonas Lemmi. I am currently enrolled in Advanced Certificate in Web and Mobile Applications Development at SDSU.
+         I am pursuing a masters in Computer Science, hopefully I can start in fall 2022.   
+         
+      </p>
+      <a href="https://github.com/Yonaskw">Please Click here to access my Github.</a>
+     </div>    
+);
+  
+
+ReactDOM.render(element, document.getElementById('contents'));
